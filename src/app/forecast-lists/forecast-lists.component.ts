51a2e338fe9041034125f6be26d58f49b0dcf467
Abdul Ripon <mdabdulripon@gmail.com>
2019-09-06 13:04:38 -0500

    create upcoming 5 days forecast update.

--- src/app/forecast-lists/forecast-lists.component.ts
+++ src/app/forecast-lists/forecast-lists.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { WeatherService } from '../services/weather.service';
 
 @Component({
   selector: 'app-forecast-lists',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forecast-lists.component.scss']
 })
 export class ForecastListsComponent implements OnInit {
+	zipcode: string;
+	forecast: any[] = [];
 
-  constructor() { }
+	constructor(
+		private _activatedRoute: ActivatedRoute, 
+		private _router: Router,
+		private _weatherService: WeatherService
+	) { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+		this._activatedRoute.params
+		.subscribe(res => {
+			this.zipcode = res.zipcode;
+			this._weatherService.getForecasts(this.zipcode).subscribe( data => {
+				this.forecast = data;
+				console.log("TCL: ForecastsListComponent -> ngOnInit ->  this.forecast",  this.forecast);
+			});
+		});
+	}
 
 }
--- src/app/forecast-lists/forecast-lists.module.ts
+++ src/app/forecast-lists/forecast-lists.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { ForecastListsRoutingModule } from './forecast-lists-routing.module';
 import { ForecastListsComponent } from './forecast-lists.component';
+import { MaterialModule } from '../shared/material/material.module';
 
 
 @NgModule({
   declarations: [ForecastListsComponent],
-  imports: [CommonModule, ForecastListsRoutingModule],
+  imports: [CommonModule, ForecastListsRoutingModule, MaterialModule],
   exports: [ForecastListsComponent]
 })
 export class ForecastListsModule {}
