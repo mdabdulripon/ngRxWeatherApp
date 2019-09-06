f6f162d0a52a28411ef6188acbcca0f1823e0346
Abdul Ripon <mdabdulripon@gmail.com>
2019-09-06 13:10:38 -0500

    Add files via upload

--- src/app/zipcode/zipcode.component.ts
+++ src/app/zipcode/zipcode.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LocationService } from '../services/location.service';
 
 @Component({
   selector: 'app-zipcode',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ZipcodeComponent implements OnInit {
 
-  constructor() { }
+    constructor(private _locationService: LocationService) { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
+
+	addLocation(event) {
+		this._locationService.addLocation(event);
+	}
 
 }
