d04daa2690f6f5d4aa4a88752d664779c6d9ea54
Abdul Ripon <mdabdulripon@gmail.com>
2019-09-06 12:58:08 -0500

    modify the loader component

--- src/app/shared/material/loader/loader.component.ts
+++ src/app/shared/material/loader/loader.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Subject } from 'rxjs';
+import { LoaderService } from './loader.service';
 
 @Component({
-  selector: 'app-loader',
-  templateUrl: './loader.component.html',
-  styleUrls: ['./loader.component.scss']
+	selector: "app-loader",
+	templateUrl: "./loader.component.html",
+	styleUrls: ["./loader.component.scss"]
 })
-export class LoaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class LoaderComponent {
+	color: 'Primary';
+	mode: 'Indeterminate';
+	value: 50;
+	isLoading$: Subject<boolean>;
+	
+	constructor(private _loaderService: LoaderService) {
+		this.isLoading$ = this._loaderService.loaderSub$;
+	}
 }
--- src/app/shared/material/loader/loader.interceptor.ts
+++ src/app/shared/material/loader/loader.interceptor.ts
@@ -1,18 +1,32 @@
 import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent} from '@angular/common/http'
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+
 import { Observable } from 'rxjs';
-import { finalize } from 'rxjs/operators';
+import { finalize } from 'rxjs/operators'
 import { LoaderService } from './loader.service';
 
+
 @Injectable()
-export class LoaderInterceptor {
-
-    constructor(private _loaderService: LoaderService) {}
-
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        this._loaderService.show();
-        return next.handle(req).pipe(
-            finalize( () => this._loaderService.hide())
+export class LoaderInterceptor implements HttpInterceptor  {
+    constructor(public loaderService: LoaderService) {};
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>{
+        this.loaderService.show();
+        return next.handle(req).pipe( 
+            finalize( () => this.loaderService.hide())
         )
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
